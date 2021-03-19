815fa67bcced1f07d5c0de12d219116bce115017
Arda Ogulcan Pektas <ardaogulcan@gmail.com>
2021-03-19 13:34:22 +0100

    feat: Remove unnecessary errors and fix package.json

--- src/useIntersectionObserver.js
+++ src/useIntersectionObserver.js
@@ -1,18 +1,7 @@
-import { useEffect } from 'react'
 import window from 'global/window'
 
 import useObserver from './useObserver'
 
 export default function useIntersectionObserver(observerOptions) {
-  useEffect(() => {
-    const isIntersectionObserverAvailable = 'IntersectionObserver' in window && 'IntersectionObserverEntry' in window
-
-    if (!isIntersectionObserverAvailable) {
-      throw new Error(
-        'IntersectionObserver is not available in this environment please consider using polyfill https://github.com/w3c/IntersectionObserver',
-      )
-    }
-  }, [])
-
   return useObserver(window.IntersectionObserver, { observerOptions })
 }
--- src/useResizeObserver.js
+++ src/useResizeObserver.js
@@ -1,18 +1,7 @@
-import { useEffect } from 'react'
 import window from 'global/window'
 
 import useObserver from './useObserver'
 
 export default function useResizeObserver() {
-  useEffect(() => {
-    const isResizeObserverAvailable = 'ResizeObserver' in window
-
-    if (!isResizeObserverAvailable) {
-      throw new Error(
-        'ResizeObserver is not available in this environment please consider using polyfill https://github.com/que-etc/resize-observer-polyfill',
-      )
-    }
-  }, [])
-
   return useObserver(window.ResizeObserver)
 }
